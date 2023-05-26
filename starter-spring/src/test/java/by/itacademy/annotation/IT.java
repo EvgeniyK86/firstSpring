834fae0f2271889ece1dc36834f85caa1f404d6b
EvgeniyK86 <129830936+EvgeniyK86@users.noreply.github.com>
2023-05-26 20:40:22 +0300

    Add files via upload

--- starter-spring/src/test/java/by/itacademy/annotation/IT.java
+++ starter-spring/src/test/java/by/itacademy/annotation/IT.java
@@ -1,5 +1,6 @@
 package by.itacademy.annotation;
 
+import by.itacademy.TestApplicationRunner;
 import by.itacademy.spring.ApplicationRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,7 +12,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@SpringBootTest(classes = ApplicationRunner.class)
+@SpringBootTest(classes = {TestApplicationRunner.class, ApplicationRunner.class})
 @ActiveProfiles("test")
 public @interface IT {
 }
